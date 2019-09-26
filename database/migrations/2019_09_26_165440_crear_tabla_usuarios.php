917d324d96581b5f18cce372e149e445656ba7f6
HectorLeo <kazama_leo19@hotmail.com>
2019-09-26 12:29:49 -0500

    tabla usuarios

---
+++ database/migrations/2019_09_26_165440_crear_tabla_usuarios.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaUsuarios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->string('correo', 100)->unique();
+            $table->string('contrasena', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+}
