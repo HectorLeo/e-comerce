33f5b36e1100365be4f62795b27726e338a45a44
HectorLeo <kazama_leo19@hotmail.com>
2019-09-26 14:24:24 -0500

    tabla empresas

---
+++ database/migrations/2019_09_26_191701_crear_tabla_empresas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CrearTablaEmpresas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->Increments('id_empresa');
+            $table->string('correo', 100);
+            $table->foreign('correo', 'fk_clientes_usuarios')->references('correo')->on('usuarios')->onDelete('restrict')->onUpdate('restrict');
+            $table->string('cuenta_bancaria_em', 50);
+            $table->string('logotipo', 50);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
