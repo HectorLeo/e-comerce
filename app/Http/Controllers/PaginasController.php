c1f41b279c4431c23e55e8491fc1ee55e3583fbd
RDavidJuarezM <nobak.153@gmail.com>
2019-09-27 13:13:50 -0500

    bootstrap

---
+++ app/Http/Controllers/PaginasController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\User;
+
+class PaginasController extends Controller
+{
+    
+    public function login()
+    {
+      return view('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'PaginasController@login' );
+
+Route::get('/login', function () {
+    return view('login');
 });
