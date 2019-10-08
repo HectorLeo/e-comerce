e6152b36c38089d8ac212f7bee7939f992d694ed
RDavidJuarezM <nobak.153@gmail.com>
2019-10-08 13:49:29 -0500

    producto

--- app/Http/Controllers/Admin/ProductosController.php
+++ app/Http/Controllers/Admin/ProductosController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 class ProductosController extends Controller
 {
     
+    public function interfaceagregar()
+    {
+      
+      //$datos = Producto::get();
+      return view('admin.admin.AgregarProducto');
+    }
     public function agregar()
     {
       return view('admin.admin.AgregarProducto');
