60c470a3c14eb542a3b335f09a54aa85d3b2a633
HectorLeo <kazama_leo19@hotmail.com>
2019-10-23 16:47:16 -0500

    primer cliente

--- resources/views/tienda/login-tienda.blade.php
+++ resources/views/tienda/login-tienda.blade.php
@@ -4,13 +4,36 @@ Inicio
 @endsection
 @section('content')
 <!-- Aqui va ell contenido--> 
-<div class="col-md-6" align="center" >
-        <div class="header-search">
-            <form>
-                <input class="input" placeholder="Correo">
-                <input class="input" placeholder="Contraseña">
-            </form>
+<div class="section" align="center">
+        <!-- container -->
+        <div class="container" align="center">
+            <!-- row -->
+            <div class="row" align="center">
+                <!-- shop -->
+                <div class="col-md-4 col-xs-6" align="center">
+                    
+                </div>
+            <div class="col-md-4 col-xs-6" align="center">
+                    <div class="newsletter"><p>Crystal <strong>Media</strong></p></div>
+                    <form method="POST" action=" {{route('loginC_post')}}">
+                        {{ csrf_field() }}
+                        <input type="email" name="email" class="input" {!! $errors->first('email','is-invalid') !!}" value="{{old('email')}}" placeholder="Correo" required="required">
+                        {!! $errors->first('email','<span class="alert alert-warning alert-dismissible">:message</span>') !!}
+                        <p></p>
+                        <input type="password" name="password" id="contrasena" class="input" {!! $errors->first('password','is-invalid') !!}" placeholder="Contraseña" required="required">
+                        {!! $errors->first('password','<span class="help-block">:message</span>') !!}
+                        <p></p>
+                        <div class="col-12" align="center">
+                            <button type="submit" class="primary-btn order-submit">Iniciar</button>
+                        </div>
+                            <!-- /.col -->
+                    </form>
+                
+            </div>
         </div>
+        <!-- /row -->
     </div>
+    <!-- /container -->
+</div>
 
 @endsection
