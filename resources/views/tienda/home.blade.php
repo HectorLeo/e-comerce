8dcfe79d23836f971b60f9f5a058b8aa0903b417
HectorLeo <kazama_leo19@hotmail.com>
2019-10-28 14:35:02 -0600

    interfaz cliente

--- resources/views/tienda/home.blade.php
+++ resources/views/tienda/home.blade.php
@@ -1,12 +1,105 @@
 @extends('layouts.app-usu')
-@section('titulohome')
-Inicio
-@endsection
 @section('iniciarsesion')
 <li><a href="{{route('loginC')}}"><i class="fa fa-user-o"></i> Iniciar sesión </a></li>
+@endsection
+@section('category')
+    @foreach ( $datosC as $item )
+        <option value="0">{{$item->nombre_c}}</option>
+    @endforeach 
+@endsection
+@section('lista')
+    @foreach ( $datosC as $item )
+        <li><a href="#">{{$item->nombre_c}}</a></li>
+    @endforeach 
 @endsection
 @section('content')
 <!-- Aqui va ell contenido> 
 
+    <!-- shop -->
+    @foreach ($datosC as $item)
+    <div class="col-md-4 col-xs-6">
+        <div class="shop">
+            <div class="shop-img">
+                <img width="100" height="250" src="{{Storage::url($item->imagen_c)}}" alt="imagen de la categoria">
+            </div>
+            <div class="shop-body">
+                <h3>{{ $item->nombre_c }}</h3>
+                <a href="#" class="cta-btn">Compra ahora <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+    </div>
+    @endforeach
+    <!-- /shop -->
+@endsection
+@section('content2')
+<div class="section">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
 
+                <!-- section title -->
+                <div class="col-md-12">
+                    <div class="section-title">
+                        <h3 class="title">Nuevos productos</h3>
+                        <!--div class="section-nav"       productos con ajax no sirve por ahora>
+                            <ul class="section-tab-nav tab-nav">
+                                <li class="active"><a data-toggle="tab" href="#tab1">Laptops</a></li>
+                                <li><a data-toggle="tab" href="#tab1">Smartphones</a></li>
+                                <li><a data-toggle="tab" href="#tab1">Cameras</a></li>
+                                <li><a data-toggle="tab" href="#tab1">Accessories</a></li>
+                            </ul>
+                        </div-->
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab1" class="tab-pane active">
+									<div class="products-slick" data-nav="#slick-nav-1">
+                                        </div>
+                                        <!-- product -->
+                                        @foreach ($datosP as $item)
+										<div class="product">
+                                                <div class="product-img">
+                                                    <img src="{{Storage::url($item->imagen_p)}}" alt="Imagen del producto">
+                                                    <div class="product-label">
+                                                        <span class="sale">-30%</span>
+                                                        <span class="new">NEW</span>
+                                                    </div>
+                                                </div>
+                                                <div class="product-body">
+                                                    <p class="product-category">Category</p>
+                                                    <h3 class="product-name"><a href="#">{{$item->nombre_p}}</a></h3>
+                                                    <h4 class="product-price">${{$item->precio_neto}}
+                                                    <div class="product-rating">
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                        <i class="fa fa-star"></i>
+                                                    </div>
+                                                    <div class="product-btns">
+                                                        <button class="quick-view"><i class="fa fa-eye"></i><span class="tooltipp">Vista Rápida</span></button>
+                                                    </div>
+                                                </div>
+                                                <div class="add-to-cart">
+                                                    <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i>añadir al carrito</button>
+                                                </div>
+                                            </div>
+                                            @endforeach
+                                            <!-- /product -->
+                                    <!-- /tab -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Products tab & slick -->
+                    </div>
+                    <!-- /row -->
+                </div>
+                <!-- /container -->
+            </div>
+            <!-- /SECTION -->
 @endsection
